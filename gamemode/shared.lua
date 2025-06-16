ecf9d806961ddec99bc457e75bfdf0c38bb14f31
Dotty Gaming <ktfireskull@gmail.com>
2025-06-16 10:17:13 -0400

    Set up simple base

---
+++ gamemode/shared.lua
@@ -0,0 +1,19 @@
+GM.Name = "BotRush"
+GM.Author = "@realdotty"
+
+BotRush = {
+	_VERSION = "1.0.0-alpha",
+
+	_CREDITS = {
+		{
+			["name"] = "Dotty",
+			["discord"] = "@realdotty",
+			["id64"] = "76561198362949858",
+			["title"] = "Project Manager"
+		}
+	}
+}
+
+function GM:Initialize()
+	-- Do stuff
+end
